ef1112fa3aa9ac7eecf3bc5c87cfea52afd944a4
Samuel <samuelcauabarbosa114@gmail.com>
2022-08-25 08:44:26 -0300

    Atualização dia 25/08 as 08:44

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  num1 : string
+  num2 : string
+  escolha : string
+  saida : string
+  saida2 : string
+  img : any = "assets/icon/favicon.png"
   constructor() {}
 
+
+calcular(){
+  var n1 = parseFloat(this.num1)
+  var n2 = parseFloat(this.num2)
+  var resultado = (n1/(n2**2)).toFixed(2)
+  if (this.escolha == "Homem"){
+    if (Number(resultado) <= 19){
+      this.saida = resultado.toString()
+      this.saida2 = "Abaixo do Peso"
+      this.img =  "assets/icon/favicon.png"
+    }
+    if (Number(resultado) > 19 && Number(resultado) < 27.3){
+      this.saida = resultado.toString()
+      this.saida2 = "Peso Ideal"
+      this.img =  "assets/icon/favicon.png"
+    }
+    if (Number(resultado) > 27.3){
+      this.saida = resultado.toString()
+      this.saida2 = "Acima do Peso"
+      this.img =  "assets/icon/alerta.jpg"
+    }
+  }
+  else{
+    this.saida = resultado.toString()
+
+  }
+  var n1 = parseFloat(this.num1)
+  var n2 = parseFloat(this.num2)
+  var resultado = (n1/(n2**2)).toFixed(2)
+  if (this.escolha == "Mulher"){
+    if (Number(resultado) <= 19){
+      this.saida = resultado.toString()
+      this.saida2 = "Abaixo do Peso"
+      this.img =  "assets/icon/favicon.png"
+    }
+    if (Number(resultado) > 19 && Number(resultado) < 27.3){
+      this.saida = resultado.toString()
+      this.saida2 = "Peso Ideal"
+      this.img =  "assets/icon/favicon.png"
+    }
+    if (Number(resultado) > 27.3){
+      this.saida = resultado.toString()
+      this.saida2 = "Acima do Peso"
+      this.img =  "assets/icon/alerta.jpg"
+    }
+  }
+  else{
+    this.saida = resultado.toString()
+
+  }
+}
 }
